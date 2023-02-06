1c550d243382411cac3c20229ff52dec72fbce54
Sofia <sofia.cronstrom@gmail.com>
2023-02-06 17:36:58 +0100

    adding data to first adding hooks in project.js, adding images to accordion in first.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,28 +1,25 @@
 import * as React from "react";
-import Sleepcycle from "../images/sleepcycle.png";
+import { useState } from 'react';
 import "../style/accordion.css"
-const Projects = ()=>{
 
-     const accordionData = {
-        image: `${Sleepcycle}`,
-        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-        Quis sapiente laborum cupiditate possimus labore, hic temporibus 
-        velit dicta earum suscipit commodi eum enim atque at? 
-        Et perspiciatis dolore iure voluptatem.`
-     };
-     console.log(accordionData)
-     const {image, content} = accordionData;
+
+const Projects = ({image, content})=>{
+
+    const [isActive, setIsActive] = useState(false);
+  
 
     return(
-     <div className="project-index">
+    
         <div className="accordion-object">
-            <div className="accordion-title">
+            <div className="accordion-title"
+            onClick={() => setIsActive(!isActive)}
+            >
                 <img style={{width: "300px"}} src={image} alt="the first page on sleepcycles investor site"/>
-                <div>+</div>
+                
             </div>
-            <div className="accordion-content">{content}</div>
+            {isActive && <div className="accordion-content">{content}</div>}
           </div>
-     </div>
+    
     )
 };
 
--- src/pages/first.js
+++ src/pages/first.js
@@ -1,16 +1,56 @@
 import * as React from "react";
+
 import About from '../components/about.js';
 import Projects from '../components/projects.js'
+
+import Sleepcycle from "../images/sleepcycle.png";
+import Mershowroom from "../images/mershowroom.png";
+import Dizzydream from "../images/dizzydream.png";
+import Weatherapp from "../images/weatherapp.png";
+
 import './first.css';
 const first = () => {
+    const accordionData = [
+    {
+        image: `${Sleepcycle}`,
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Quis sapiente laborum cupiditate possimus labore, hic temporibus 
+        velit dicta earum suscipit commodi eum enim atque at? 
+        Et perspiciatis dolore iure voluptatem.`
+     },
+     {
+        image: `${Mershowroom}`,
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Quis sapiente laborum cupiditate possimus labore, hic temporibus 
+        velit dicta earum suscipit commodi eum enim atque at? 
+        Et perspiciatis dolore iure voluptatem.`
+     },
+     {
+        image: `${Dizzydream}`,
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Quis sapiente laborum cupiditate possimus labore, hic temporibus 
+        velit dicta earum suscipit commodi eum enim atque at? 
+        Et perspiciatis dolore iure voluptatem.`
+     },
+     {
+        image: `${Weatherapp}`,
+        content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Quis sapiente laborum cupiditate possimus labore, hic temporibus 
+        velit dicta earum suscipit commodi eum enim atque at? 
+        Et perspiciatis dolore iure voluptatem.`
+     }
 
-
+    ];
     return (  
         
         <React.Fragment>
           <About/>
-          <Projects/>
-       </React.Fragment>
+          <div className="project-index">
+                {accordionData.map(({ image, content }) => (
+                <Projects image={image} content={content}/>
+                ))}
+         </div>
+        </React.Fragment>
    
     );
 }
