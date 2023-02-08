61e6eaf01860805d9abf1ee6191e59b36df1f630
Sofia <sofia.cronstrom@gmail.com>
2023-02-08 17:29:14 +0100

    adding animations to accordion-objects

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,19 +5,31 @@ import "../style/accordion.css"
 
 const Projects = ({image, content})=>{
 
-    const [isActive, setIsActive] = useState(false);
-  
+    const [isActive, setIsActive] = useState("");
+    const [isOpacity, setIsOpacity] = useState("0");
+    const [isVisibility, setIsVisibility] = useState("hidden")
+    
+    function toggleAccordion() {
+        setIsActive(isActive === "" ? "active" : "");
+        setIsOpacity(
+          isActive === "active" ? "0" : "1"
+        );
+        setIsVisibility(
+           isActive === "active" ? "hidden" : "visible"
+        );
+    }
 
     return(
     
         <div className="accordion-object">
-            <div className="accordion-title"
-            onClick={() => setIsActive(!isActive)}
+            <div className={`accordion-title ${isActive}`}
+            onClick={toggleAccordion}
             >
-                <img style={{width: "300px"}} src={image} alt="the first page on sleepcycles investor site"/>
+                <img style={{width: "400px"}} src={image} alt="the first page on sleepcycles investor site"/>
                 
             </div>
-            {isActive && <div className="accordion-content">{content}</div>}
+            <div className="accordion-content"
+                  style={{opacity: `${isOpacity}`, visibility: `${isVisibility}`}}>{content}</div>
           </div>
     
     )
