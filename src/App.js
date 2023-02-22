7e7b07b54b4f1950aef75cd1c0fa2f2432d65749
Sofia <sofia.cronstrom@gmail.com>
2023-02-22 16:12:25 +0100

    adding a new project at app.js

--- src/App.js
+++ src/App.js
@@ -2,45 +2,52 @@
 import * as React from "react"
 
 import About from './components/about.js';
-import Projects from './components/projects.js'
+import Projects from './components/projects.js';
+
 import Sleepcycle from "./images/sleepcycle.png";
 import Mershowroom from "./images/mershowroom.png";
 import Dizzydream from "./images/dizzydream.png";
 import Weatherapp from "./images/weatherapp.png";
+import Meltdown from './images/meltdown.png';
 
-import './pages/first.css';
+import './style/first.css';
 
 const App = () => {
   
 
   const accordionData = [
     {
-        id: "item1",
+      id: "item1",
+      image: `${Meltdown}`,
+      heading: "Website improvements",
+      link: "https://www.meltdown-mgmt.com/sv"
+   },
+    {
+        id: "item2",
         image: `${Sleepcycle}`,
         heading: "Sleep Cycle, inv. site",
         link: "https://investors.sleepcycle.com/"
      },
      {
-        id: "item2",
+        id: "item3",
         image: `${Mershowroom}`,
         heading: "Virtual showroom",
         content: "https://github.com/SofiaCronstrom/MER_showroom",
         link: "https://mershowroom.netlify.app/"
      },
      {
-        id: "item3",
+        id: "item4",
         image: `${Dizzydream}`,
         heading: "WebVR game",
         content: "https://github.com/SofiaCronstrom/dizzy-dream",
         link: "https://sofiacronstrom.github.io/dizzy-dream/"
      },
      {
-        id: "item4",
+        id: "item5",
         image: `${Weatherapp}`,
         heading: "Weather app",
         content: "https://github.com/SofiaCronstrom/weather-app-async"
      }
-  
 
     ];
   
